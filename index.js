f940ebbc9122c47edcb75180b23dbd58d77713fe
Rohrbacher Léon <leon.rohrbacher8@etu.univ-lorraine.fr>
2018-11-16 16:12:05 +0100

    starting point of a server but issue with routing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//init files
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const router = express.Router();
+
+app.set('views', __dirname + '/src/views');
+app.set('view_engine', 'twig');
+
+router.get('/home', (req, res) => {
+    res.render('HomeView');
+});
+
+server.listen(8080, function(){
+    console.log("Server running");
+});
