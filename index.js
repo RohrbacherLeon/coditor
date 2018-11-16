3fefd522f83d3c65bf04eccb96af704e8edb27d5
Rohrbacher Léon <leon.rohrbacher8@etu.univ-lorraine.fr>
2018-11-16 17:17:49 +0100

    server not rendering properly

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const router = express.Router();
 
-app.set('views', __dirname + '/src/views');
-app.set('view_engine', 'twig');
+//const router = express.Router();
+//const body_parser = require('body-parser');
 
-router.get('/home', (req, res) => {
+app.set('views', __dirname + '\\src\\views');
+//app.set('assets', __dirname + '\\assets');
+app.set('view engine', 'twig');
+
+
+app.get('/home', (req, res) => {
     res.render('HomeView');
 });
 
