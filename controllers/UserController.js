09db94ee3915eb3db74ea62c5159e0c856948348
Anthony Zink <anthony.zink@outlook.fr>
2018-12-02 18:15:08 +0100

    ajout de la connexion d'un utilisateur

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
  * When register is reached with post method
  */
 exports.register_post = (req, res) => {
-    
+
     req.checkBody('first_name', 'Username is required').notEmpty();
     req.checkBody('last_name', 'Username is required').notEmpty();
     req.checkBody('email', 'Username is required').notEmpty();
@@ -16,7 +16,7 @@ exports.register_post = (req, res) => {
 	else {
         //test si l'email n'est deja pas utilise
         User.findOne({ email: { "$regex": "^" + req.body.email + "\\b", "$options": "i"} }, (err, user) => {
-            //si email deja utilisé, on ridirige vers la page register 
+            //si email deja utilisé, on redirige vers la page register 
             if (user) 
                 res.render('RegisterView');
             //si email pas utilisé, on créé l'utilisateur et on le redirige vers le login
@@ -24,7 +24,6 @@ exports.register_post = (req, res) => {
 				User.createUser(req.body,(err, user) => {
 					if (err) throw err;
                 });
-
                 res.redirect('/login');
             }
 		});
@@ -38,4 +37,20 @@ exports.register_post = (req, res) => {
  */
 exports.register_get = (req, res) => {
     res.render('RegisterView');
+}
+
+/**
+ * When logout is reached with get method
+ */
+exports.logout = (req, res) => {
+    req.logout();
+	req.flash('success_msg', 'You are logged out');
+	res.redirect('/login');
+}
+
+/**
+ * When login is reached with get method
+ */
+exports.login_get = (req, res) => {
+    res.render('LoginView');
 }
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const UserController = require('../controllers/UserController')
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/User');
+
 router.get('/', (req, res) => {
     res.render('HomeView');
 });
 
-router.get('/login', (req, res) =>{
-    res.render('LoginView');
-});
+router.get('/login', UserController.login_get);
 
 
 router.get('/register', UserController.register_get);
@@ -19,5 +22,41 @@ router.post('/register', UserController.register_post);
 router.get('/forgot_password', (req, res) => {
     res.render('ForgotPasswordView');
 });
+
+passport.use(new LocalStrategy({usernameField: 'email'},
+	function (username, password, done) {
+		User.getUserByEmail(username, function (err, user) {
+			if (err) throw err;
+			if (!user) {
+				return done(null, false, { message: 'Unknown User' });
+			}
+
+			User.comparePassword(password, user.password, function (err, isMatch) {
+				if (err) throw err;
+				if (isMatch) {
+					return done(null, user);
+				} else {
+					return done(null, false, { message: 'Invalid password' });
+				}
+			});
+		});
+	})
+);
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true }),
+	(req, res) => { res.redirect('/'); }
+);
+
+router.get('/logout', UserController.logout);
 
 module.exports = router;
