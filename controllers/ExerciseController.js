b4e8ae11dca66f4c45d4e37c3528034a71f6acab
Anthony Zink <anthony.zink@outlook.fr>
2018-12-17 14:05:00 +0100

    refactoring recherche par tags

--- controllers/ExerciseController.js
+++ controllers/ExerciseController.js
@@ -1,9 +1,8 @@
 const Exercise = require('../models/Exercise')
-const express    = require('express');
-const app        = express();
 
 exports.getExoByLang = (req, res) => {
-    Exercise.byLanguage(req.params.lang,function(err, exos){
-        res.render('BrowsingView', {datas : exos});
+    Exercise.getAllValuesOf('language',function(err, languages){
+        let locale = req.params.lang;
+        res.render('BrowsingView', {languages, locale});
     })
 }
