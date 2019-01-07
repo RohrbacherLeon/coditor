f42e220580cb47302ff16da96dc911f9a9f815c0
Florian <florian.cordier2@gmail.com>
2019-01-07 09:49:58 +0100

    Préparation connection github google

---
+++ routes/middlewares/GithubConnection.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-github').Strategy;
+
+const User = require('../../models/User');
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+module.exports = passport;
---
+++ routes/middlewares/GoogleConnection.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-google-oaut').OAuthStrategy;
+
+const User = require('../../models/User');
+
+passport.use(new GoogleStrategy({
+    consumerKey: GOOGLE_CONSUMER_KEY,
+    consumerSecret: GOOGLE_CONSUMER_SECRET,
+    callbackURL: "http://www.example.com/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+	User.getUserById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+module.exports = passport;
