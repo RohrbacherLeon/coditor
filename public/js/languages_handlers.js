afa985a81f04b1470c5e0c6f4320b137cbcc47b0
Rohrbacher Léon <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-01-10 18:17:17 +0100

    class active switch now working

--- public/js/languages_handlers.js
+++ public/js/languages_handlers.js
@@ -1,7 +1,7 @@
 let base_url = "http://localhost:3000/";
 let slot = $('.exercises');
 
-printExercisesForLanguage = (lang) => {
+printExercisesForLanguage = function(lang){
     axios.get(base_url + `api/languages/${lang}`).then(function(response){
         let html = "";
         response.data.forEach(exercise => {
@@ -12,12 +12,23 @@ printExercisesForLanguage = (lang) => {
         });
         slot.empty();
         slot.append(html);
-        console.log(response.data);
     }).catch(function(error){
         if(error)
             throw error;
     });
 }
 
-$('#lang_js').click(() => { printExercisesForLanguage('js') });
-$('#lang_php').click(() => { printExercisesForLanguage('php') });
+setActive = function(elem){
+    elem.siblings().removeClass('active');
+    elem.attr('class', 'active');
+}
+
+$('#lang_js').click(function() {
+    printExercisesForLanguage('js');
+    setActive($(this));
+});
+
+$('#lang_php').click(function() {
+    printExercisesForLanguage('php');
+    setActive($(this));
+});
