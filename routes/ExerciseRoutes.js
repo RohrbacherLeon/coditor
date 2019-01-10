465ae5fae0da087c193f666e8f4c4bbf02a8da90
Rohrbacher Léon <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-01-10 18:23:50 +0100

    disabled url routes for old system

--- routes/ExerciseRoutes.js
+++ routes/ExerciseRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const ExerciseController = require('../controllers/ExerciseController')
+const Exercise = require('../models/Exercise');
 
 router.get('/', (req, res)=> {
-    res.redirect('/exercises/js')
+    Exercise.getAllValuesOf('tags', (err, tags) => {
+        res.render('BrowsingView',  {tags});
+    });
+    //res.redirect('/exercises/js')
 });
 
-router.get('/:lang', ExerciseController.getExoByLang);
+//router.get('/:lang', ExerciseController.getExoByLang);
 
 router.get('/:lang/:slug', ExerciseController.getExercise);
 router.post('/:lang/:slug', ExerciseController.postExercise);
