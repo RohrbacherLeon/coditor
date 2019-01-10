9a7a4c2d22124a84efad6867994fd2e86771e6ac
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-01-10 10:27:45 +0100

    added 3rd 4th test spec

--- cypress/integration/2_login_spec.js
+++ cypress/integration/2_login_spec.js
@@ -1,51 +1,45 @@
-let urlStart = "localhost:3000";
-
 describe('Test Login Local', function() {
 
-  //Test if page is visitable
-  it('Visits the Login page', function() {
-    cy.visit(urlStart+'/login');
-  });
+    //Test if page is visitable
+    it('Visits the Login page', function() {
+        cy.visit(Cypress.env('baseUrl') + '/login');
+    });
 
-  //Test wrong email
-  it('Test wrong email', function() {
-    cy.get('input[name=email]').type("TestEmail@wrongEmail.com");
-    cy.get('input[name=password]').type("testpass");
+    //Test wrong email
+    it('Test wrong email', function() {
+        cy.get('input[name=email]').type("TestEmail@wrongEmail.com");
+        cy.get('input[name=password]').type("testpass");
 
-    cy.get(".button.button--green").click();
+        cy.get(".button.button--green").click();
 
-    //TESTING ERROR MESSAGE
-    cy.contains(".alert.alert-danger", "Adresse email ou mot de passe incorrecte.");
+        //TESTING ERROR MESSAGE
+        cy.contains(".alert.alert-danger", "Adresse email ou mot de passe incorrecte.");
 
-    cy.url().should('include', '/login');
-  });
+        cy.url().should('include', '/login');
+    });
 
-  //Test wrong password
-  it('Test wrong password', function() {
-    cy.wait(500);
-    cy.get('input[name=email]').clear();
-    cy.get('input[name=email]').type("email@coditor.fr");
-    cy.get('input[name=password]').type("testpassWRONG");
+    //Test wrong password
+    it('Test wrong password', function() {
+        cy.get('input[name=email]').clear();
+        cy.get('input[name=email]').type("email@coditor.fr");
+        cy.get('input[name=password]').type("testpassWRONG");
 
-    cy.get(".button.button--green").click();
+        cy.get(".button.button--green").click();
 
-    //TESTING ERROR MESSAGE
-    cy.contains(".alert.alert-danger", "Adresse email ou mot de passe incorrecte.");
+        //TESTING ERROR MESSAGE
+        cy.contains(".alert.alert-danger", "Adresse email ou mot de passe incorrecte.");
 
-    cy.url().should('include', '/login');
-  });
+        cy.url().should('include', '/login');
+    });
 
-  //Success login
-  it('Test success login', function() {
-    cy.wait(500);
-    cy.get('input[name=email]').clear();
-    cy.get('input[name=email]').type("email@coditor.fr");
-    cy.get('input[name=password]').type("testpass");
+    //Success login
+    it('Test success login', function() {
+        cy.get('input[name=email]').clear();
+        cy.get('input[name=email]').type("email@coditor.fr");
+        cy.get('input[name=password]').type("testpass");
 
-    cy.wait(500);
-    cy.get(".button.button--green").click();
-    cy.wait(500);
-    cy.url().should('contain', urlStart);
-    cy.contains(".alert.alert-success", 'Vous êtes maintenant connecté.');
-  });
+        cy.get(".button.button--green").click();
+        cy.url().should('contain', urlStart);
+        cy.contains(".alert.alert-success", 'Vous êtes maintenant connecté.');
+    });
 });
--- cypress/integration/3_home_spec.js
+++ cypress/integration/3_home_spec.js
@@ -1,8 +1,35 @@
-let urlStart = "localhost:3000";
-
 describe('Testing home page', function() {
-    //Testing page visit
+
     it('Page visit', function() {
-        cy.visit(urlStart + '/');
-    })
+        cy.visit('/');
+    });
+
+    it("Menu logo link", function() {
+        cy.get('.nav__logo').click();
+        cy.url().should('equal', Cypress.env('baseUrl') + "/");
+    });
+
+    it("Menu login link", function() {
+        cy.get('.nav__right').children('a').click();
+        cy.url().should('equal', Cypress.env('baseUrl') + "/login");
+    });
+
+    it("Menu's dropdown profile link", function() {
+        cy.login();
+        cy.get('a[href=/profile]').click();
+        cy.url().should('equal', Cypress.env('baseUrl') + "/profile");
+    });
+
+    it("Menu's dropdown exercises series link", function() {
+        cy.login();
+        cy.get('a[href=/series]').click();
+        cy.url().should('equal', Cypress.env('baseUrl') + "/series");
+    });
+
+    it("Logout button", function() {
+        cy.login();
+        cy.get('a[href=/logout]').click();
+        cy.url().should('equal', Cypress.env('baseUrl') + "/");
+    });
+
 });
