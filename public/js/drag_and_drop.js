58d724477f34ca81fa95ecda4ea66b771fdc4a24
Alexduqeb <alexandre.ralli8@etu.univ-lorraine.fr>
2019-01-11 14:37:45 +0100

    Ajout droppable dans la div initiale

--- public/js/drag_and_drop.js
+++ public/js/drag_and_drop.js
@@ -23,3 +23,24 @@ out: function(event, elem) {
     $(this).removeClass("over");
 }
 });
+
+$(".browser").droppable({
+    accept: ".draggable",
+    drop: function(event, ui) {
+        console.log("drop");
+        $(this).removeClass("over");
+        var dropped = ui.draggable;
+        var droppedOn = $(this);
+        $(dropped).detach().css({
+        top: 0,
+        left: 0
+        }).appendTo(droppedOn);
+    },
+    over: function(event, elem) {
+        $(this).addClass("over");
+        console.log("over");
+    },
+    out: function(event, elem) {
+        $(this).removeClass("over");
+    }
+    });
