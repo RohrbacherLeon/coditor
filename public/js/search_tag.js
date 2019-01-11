e50727523179843917c5389b1071b9413ab2b9d4
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-01-11 15:36:00 +0100

    first part of refactor _ most functionnalities are back

--- public/js/search_tag.js
+++ public/js/search_tag.js
@@ -1,45 +1,27 @@
-$(document).ready(function() {
-  $('#searchByTag').select2({ width: '100%' });
-});
-
-$.get(`/api/exercises`).then(function(data){  
-  generateExercises(data);
+$.get(`/api/exercises`).then(function(data) {
+    generateExercises(data);
 })
 
-$('#searchByTag').on('select2:select', function (e) {
-   let tags = [];
+$('#searchByTag').on('select2:select', function(e) {
+    let tags = [];
 
-   $(e.target).select2('data').forEach(tag => {
-     tags.push(tag.text);
-   });
+    $(e.target).select2('data').forEach(tag => {
+        tags.push(tag.text);
+    });
 
-  $.get(`/api/exercises?lang=${window.location.href.split('/').reverse()[0]}&tags=${tags.join(',')}`).then(function(data){
-    console.log(data);
-    generateExercises(data)
-  })
+    $.get(`/api/exercises?lang=${window.location.href.split('/').reverse()[0]}&tags=${tags.join(',')}`).then(function(data) {
+        console.log(data);
+        generateExercises(data)
+    })
 });
 
-$('#searchByTag').on('select2:unselect', function (e) {
-  let tags = [];
-  $(e.target).select2('data').forEach(tag => {     
-    tags.push(tag.text);
-  });  
+$('#searchByTag').on('select2:unselect', function(e) {
+    let tags = [];
+    $(e.target).select2('data').forEach(tag => {
+        tags.push(tag.text);
+    });
 
-  $.get(`/api/tags/filter?lang=${window.location.href.split('/').reverse()[0]}&tags=${tags.join(',')}`).then(function(data){    
-    generateExercises(data)
-  })
+    $.get(`/api/tags/filter?lang=${window.location.href.split('/').reverse()[0]}&tags=${tags.join(',')}`).then(function(data) {
+        generateExercises(data)
+    })
 });
-
-function generateExercises(data){
-  let exercices = "";
-   data.forEach(exercice => {
-     exercices += `
-     <a href="/exercises/${exercice.language}/${exercice.slug}" class="exercise ${exercice.language} draggable">
-         <h4>${exercice.title}</h4>
-         <p>${exercice.author}</p>
-     </a>
-     `
-   });
-   $('.exercises').empty();
-   $('.exercises').append(exercices);
-}
