a9a5ce81df8dc9e53240b6841c186540dfff70ae
Florian <florian.cordier2@gmail.com>
2019-01-11 16:03:49 +0100

    Refactor (ProfileController, .gitignore)

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -4,6 +4,7 @@ const formidable = require('formidable'),
     path = require('path'),
     slugify = require('slugify')
 
+    
 exports.getProfile = (req, res) =>{
     if(req.user.type == "teacher"){
         Exercise.countDocuments({author:req.user.profile.email}, function (err, count) {
@@ -18,6 +19,35 @@ exports.getCreateExercise = (req, res) =>{
     Exercise.getAllValuesOf('tags', (err, tags) => {
         res.render('CreateExerciseView', {tags});
     })
+}
+
+/**
+ * Function used to save a file (Test, Correction and Skeleton). Return "ok" if no error.
+ * @param {*} file the file
+ * @param {*} repertory repertory where the file are save.
+ * @param {*} slug 
+ * @param {*} res 
+ */
+function saveFile(file, repertory, slug, res){
+        let old_path = file.path,
+        file_size = file.size,
+        file_ext = file.name.split('.').pop(),
+        index = old_path.lastIndexOf('/') + 1,
+        file_name = old_path.substr(index),
+        
+        //Save test file to the folder tests
+        new_path = path.join(process.cwd(), '/' + repertory + '/', slug + '.' + file_ext);
+    fs.readFile(old_path, function(err, data) {
+        fs.writeFile(new_path, data, function(err) {
+            fs.unlink(old_path, function(err) {
+                if(err){
+                    res.render('CreateExerciseView', {message : "Erreur lors de la création de l'exercice."});
+                }else{
+                    return "ok";
+                }
+            });
+        });
+    });
 }
 
 exports.postCreateExercise = (req, res) =>{
@@ -40,85 +70,37 @@ exports.postCreateExercise = (req, res) =>{
         //penser a verifer l'extension du fichier de test
         //trier les fichiers de test dans un dossier nommer par l'email de l'auteur ?
         
-        //verification qu'il y est bien un fichier de test
-        let old_path_test;
+        //if a file "Test" exist
+        let test_file;
         if(files.file.size > 0){
-                old_path_test = files.file.path;
-
-                let file_size_test = files.file.size,
-                file_ext_test = files.file.name.split('.').pop(),
-                index_test = old_path_test.lastIndexOf('/') + 1,
-                file_name_test = old_path_test.substr(index_test),
-                
-                //Save test file to the folder tests
-                new_path_test = path.join(process.cwd(), '/tests/', slug + '.' + file_ext_test);
-            fs.readFile(old_path_test, function(err, data) {
-                fs.writeFile(new_path_test, data, function(err) {
-                    fs.unlink(old_path_test, function(err) {
-                        if(err){
-                            res.render('CreateExerciseView', {message : "Erreur lors de la création de l'exercice."});
-                        }
-                    });
-                });
-            });
+            test_file = saveFile(files.file, "tests", slug, res);
         }else{
             res.render('CreateExerciseView', {message : "Aucun fichier de test donné."});
         }
 
         //test if correction exist
-        let old_path_correction;
+        let correction_file;
         if(files.file_correction.size > 0){
-                old_path_correction = files.file_correction.path;
-                let file_size_correction = files.file_correction.size,
-                file_ext_correction = files.file_correction.name.split('.').pop(),
-                index_correction = old_path_correction.lastIndexOf('/') + 1,
-                file_name_correction = old_path_correction.substr(index_correction),
-
-                //Save correction file to the folder corrections
-                new_path_correction = path.join(process.cwd(), '/corrections/', slug + '.' + file_ext_correction);
-            fs.readFile(old_path_correction, function(err, data) {
-                fs.writeFile(new_path_correction, data, function(err) {
-                    fs.unlink(old_path_correction, function(err) {
-                        if(err){
-                            res.render('CreateExerciseView', {message : "Erreur lors de la création de l'exercice."});
-                        }
-                    });
-                });
-            });
+            correction_file = saveFile(files.file_correction, "corrections", slug, res);
         }
-        //test if skeleton exist
-        let old_path_skeleton;
-        if(files.file_skeleton.size > 0){
-                old_path_skeleton = files.file_skeleton.path;
-                let file_size_skeleton = files.file_skeleton.size,
-                file_ext_skeleton = files.file_skeleton.name.split('.').pop(),
-                index_skeleton = old_path_skeleton.lastIndexOf('/') + 1,
-                file_name_skeleton = old_path_skeleton.substr(index_skeleton),
 
-                //Save correction file to the folder skeletons
-                new_path_skeleton = path.join(process.cwd(), '/skeletons/', slug + '.' + file_ext_skeleton);
-            fs.readFile(old_path_skeleton, function(err, data) {
-                fs.writeFile(new_path_skeleton, data, function(err) {
-                    fs.unlink(old_path_skeleton, function(err) {
-                        if(err){
-                            res.render('CreateExerciseView', {message : "Erreur lors de la création de l'exercice."});
-                        }
-                    });
-                });
-            });
+        //test if skeleton exist
+        let skeleton_file;
+        if(files.file_skeleton.size > 0){
+            skeleton_file = saveFile(files.file_skeleton, "skeletons", slug, res);
         }
 
         //Return
         let messageCreation = "";
-        if(old_path_test != null)
+        if(test_file != null)
         {
             messageCreation = "Exercice créé";
         }
-        if(old_path_correction != null)
+        if(correction_file != null)
         {
             messageCreation += ", avec correction";
         }
-        if(old_path_skeleton != null)
+        if(skeleton_file != null)
         {
             messageCreation += ", avec squelette";
         }
