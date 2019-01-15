bf1a5e30576ae424073eff1b8284108224cbaff1
Alexduqeb <alexandre.ralli8@etu.univ-lorraine.fr>
2019-01-15 15:05:19 +0100

    drop avec fleche et bouton plus

--- public/js/browsing_handlers.js
+++ public/js/browsing_handlers.js
@@ -1,6 +1,8 @@
 let base_url = "http://localhost:3000/";
 let slot = $('.exercises');
 let current_language = "all";
+let arrow_right="<img src='/images/arrow-right.png' class='arrow-right'>";
+
 
 switchLanguage = function(li_elem) {
     current_language = li_elem.attr('id').split('_', 2)[1];
@@ -36,14 +38,14 @@ function refreshTags(e) {
 function refreshExercises(exercises_html) {
     slot.empty();
     slot.append(exercises_html);
-}
-
-$.get(`/api/tags/filter?lang=${current_language}`).then(function(data) {
-    refreshExercises(getExercisesHtml(data));
     $(".draggable").draggable({
         cursor: "move",
         revert: "invalid"
     });
+}
+
+$.get(`/api/tags/filter?lang=${current_language}`).then(function(data) {
+    refreshExercises(getExercisesHtml(data));
 });
 
 
@@ -51,14 +53,16 @@ $.get(`/api/tags/filter?lang=${current_language}`).then(function(data) {
 $("#drop_zone").droppable({
     accept: ".draggable",
     drop: function(event, ui) {
-        console.log("drop");
         $(this).removeClass("over");
         var dropped = ui.draggable;
         var droppedOn = $(this);
+        $(arrow_right).prependTo(droppedOn);
         $(dropped).detach().css({
         top: 0,
         left: 0
-        }).appendTo(droppedOn);
+        }).prependTo(droppedOn);
+        
+
     },
     over: function(event, elem) {
         $(this).addClass("over");
