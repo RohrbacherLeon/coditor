48b1645635bf7c12caf1652e7a6a707648c99e3a
Anthony Zink <anthony.zink@outlook.fr>
2019-01-21 13:31:20 +0100

    refactor des settings

--- routes/ProfileRoutes.js
+++ routes/ProfileRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const ProfileController = require('../controllers/ProfileController')
-const {isTeacher, isConnectedWithLocalAccount} = require('./middlewares/Authenticated')
+const {isTeacher} = require('./middlewares/Authenticated')
 
 /**
  * Get the profile of the user
@@ -23,12 +23,6 @@ router.post('/create-exercise', isTeacher, ProfileController.postCreateExercise)
  */
 router.get('/create-exercises-set', isTeacher, ProfileController.getCreateExercisesSet);
 
-/**
- * Get the settings page
- */
-router.get('/settings', isConnectedWithLocalAccount, ProfileController.getSettings);
-router.post('/settings/global', isConnectedWithLocalAccount,ProfileController.postSettingsGlobal);
-router.post('/settings/password', isConnectedWithLocalAccount,ProfileController.postSettingsPassword);
-router.post('/settings/delete_account', isConnectedWithLocalAccount,ProfileController.postSettingsDeleteAccount);
+
 
 module.exports = router;
---
+++ routes/SettingsRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const {isConnectedWithLocalAccount} = require('./middlewares/Authenticated')
+const SettingsController = require('../controllers/SettingsController')
+
+
+/**
+ * Get the settings page
+ */
+router.get('/', isConnectedWithLocalAccount, SettingsController.getSettings);
+router.post('/global', isConnectedWithLocalAccount,SettingsController.postSettingsGlobal);
+router.post('/password', isConnectedWithLocalAccount,SettingsController.postSettingsPassword);
+router.post('/delete_account', isConnectedWithLocalAccount,SettingsController.postSettingsDeleteAccount);
+
+module.exports = router;
