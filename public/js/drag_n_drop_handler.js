d12518d1232a5075601b5c21e96311cc1da2de17
Alexduqeb <alexandre.ralli8@etu.univ-lorraine.fr>
2019-03-25 17:00:21 +0100

    Fix bug droppable sur liste exercises : création de deux fichiers séparés

--- public/js/drag_n_drop_handler.js
+++ public/js/drag_n_drop_handler.js
@@ -42,30 +42,51 @@ function refreshTags(e) {
 function refreshExercises(exercises_html) {
     slot.empty();
     slot.append(exercises_html);
+    
 }
 
 $(document).ready(function() {
-    $.get(`/api/tags/filter?lang=${current_language}`).then(function(data) {
-        refreshExercises(getExercisesHtml(data));
+
+
+    $(".draggable").draggable({
+        cursor: "move",
+        revert: "invalid"
     });
 
 
-    $('#searchByTag').select2({ width: '100%' });
-
-    $('#lang_js').click(function() {
-        switchLanguage($(this));
+    // Drop script
+    $("#drop_zone").droppable({
+        accept: ".draggable",
+        drop: function(event, ui) {
+            $(this).removeClass("over");
+            var dropped = ui.draggable;
+            exercises_selected.push(dropped[0].id);
+            $("#exercises_selected").val(exercises_selected);
+            var droppedOn = $(this);
+            $(dropped).detach().css({
+                top: 0,
+                left: 0
+            }).appendTo(droppedOn).draggable( "disable" ).addClass("selected");
+            $(delete_arrow+arrow_right).appendTo(droppedOn);
+            $('#plus_button').appendTo(droppedOn);
+            
+        },
+        over: function(event, elem) {
+            $(this).addClass("over");
+        },
+        out: function(event, elem) {
+            $(this).removeClass("over");
+        }
     });
 
-    $('#lang_php').click(function() {
-        switchLanguage($(this));
+    $('#drop_zone').on('click', '.delete_arrow', function(e) {
+        exercises_selected.splice($.inArray($(this).prev()[0].id, exercises_selected),1);
+        $("#exercises_selected").val(exercises_selected);
+        $(this).next().remove();
+        $(this).prev().remove();
+        $(this).remove();
+        $.get(`/api/tags/filter?lang=${current_language}`).then(function(data) {
+            refreshExercises(getExercisesHtml(data));
+        });
     });
-
-    $('#searchByTag').on('select2:select', function(e) {
-        refreshTags(e);
-    });
-
-    $('#searchByTag').on('select2:unselect', function(e) {
-        refreshTags(e);
-    });
-
 });
