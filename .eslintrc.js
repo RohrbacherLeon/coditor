2b43e49172021acbcb803fae448fe02de4405156
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-03-25 09:46:07 +0100

    updated linter for quotes free use

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
 
     "rules": {
         "semi": ["error", "always"],
-        "quotes": ["error", "double"],
+        "quotes": ["error", "double", "simple", "backtick"],
         "indent": ["off"],
         "no-control-regex": ["off"],
         "no-tabs": ["error", { allowIndentationTabs: true }]
