a17ca2affc567d99321b38071c15f35b4fa2b42c
Anthony zink <anthony.zink@outlook.fr>
2019-03-30 21:50:55 +0100

    stats exos

--- controllers/ExerciseController.js
+++ controllers/ExerciseController.js
@@ -91,12 +91,12 @@ exports.postExercise = (req, res) => {
                         // // Génére un fichier nameFile de test (ajoute la fonction étudiante dans le fichier test du prof)
                         nameFile = Generator.generateJS(data, fct, req.params.slug);
                         dockerCommande = `docker run --rm -v $(pwd)/tmp:/app/tmp coditor-js node nodescript.js tmp/${nameFile}`;
-                        executeDocker(req, res, nameFile, dockerCommande, exo.language);
+                        executeDocker(req, res, nameFile, dockerCommande, exo);
                         break;
                     case "php":
                         nameFile = Generator.generatePHP(data, fct, req.params.slug);
                         dockerCommande = `docker run --rm -v $(pwd)/tmp:/app/tests/ coditor-php vendor/bin/phpunit --testdox tests/${nameFile}`;
-                        executeDocker(req, res, nameFile, dockerCommande, exo.language);
+                        executeDocker(req, res, nameFile, dockerCommande, exo);
                         break;
                     default:
                         break;
@@ -106,7 +106,7 @@ exports.postExercise = (req, res) => {
     }
 };
 
-function executeDocker (req, res, nameFile, commande, lang) {
+function executeDocker (req, res, nameFile, commande, exo) {
     exec(commande, (error, stdout, stderr) => {
         if (error) {
             fs.unlinkSync(process.cwd() + `/tmp/${nameFile}`);
@@ -119,7 +119,7 @@ function executeDocker (req, res, nameFile, commande, lang) {
                 language: req.params.lang
             };
             let analyse = {};
-            switch (lang) {
+            switch (exo.language) {
                 case "js":
                     analyse = analyseJS(stdout);
                     break;
@@ -129,7 +129,22 @@ function executeDocker (req, res, nameFile, commande, lang) {
             }
 
             if (analyse.total === analyse.success.length) {
-                updateScore(req, lang);
+                updateScore(req, exo.language);
+                let stat = {
+                    success: exo.stats.success + 1,
+                    fails: exo.stats.fails
+                };
+                Exercise.findOneAndUpdate(exo._id, { $set: { stats: stat } }, function (err, res) {
+                    if (err) console.log(err);
+                });
+            } else {
+                let stat = {
+                    success: exo.stats.success,
+                    fails: exo.stats.fails + 1
+                };
+                Exercise.findOneAndUpdate(exo._id, { $set: { stats: stat } }, function (err, res) {
+                    if (err) console.log(err);
+                });
             }
 
             showExercice(query, req, res, analyse.success);
