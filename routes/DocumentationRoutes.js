7010999b7f26ee83d898b84c26117025b7883f32
Rohrbacher Léon <leon.rohrbacher8@etu.univ-lorraine.fr>
2019-04-04 11:00:31 +0200

    documentation pages

--- routes/DocumentationRoutes.js
+++ routes/DocumentationRoutes.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("DocumentationView.twig");
+    res.render("doc/DocGeneralView.twig");
+});
+
+router.get("/js", (req, res) => {
+    res.render("doc/DocJsView.twig");
+});
+
+router.get("/php", (req, res) => {
+    res.render("doc/DocPhpView.twig");
 });
 
 module.exports = router;
